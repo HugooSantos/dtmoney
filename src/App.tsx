e7db5bbad132efc18454aa20d9da47c265ece133
HugooSantos <hugogabrieltjj@gmail.com>
2022-04-15 22:29:41 -0300

    add axios to request routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
-
+Modal.setAppElement('#root')
 
 function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransanctionModal(){
+      setIsNewTransactionModalOpen(true)
+  }
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransanctionModal} />
       <Dashboard />
+      <Modal
+      isOpen={isNewTransactionModalOpen} 
+      onRequestClose={handleCloseNewTransactionModal}
+      >
+          <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
